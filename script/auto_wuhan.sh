b869a6ddbf418f6ffb2771a09b134d7909163c4f
liyp0095 <liyp0095@gmail.com>
2020-02-08 13:31:02 -0600

    which python

--- script/auto_wuhan.sh
+++ script/auto_wuhan.sh
@@ -1,7 +1,8 @@
 time_now=`date +%s`
 
-source ~/.bashrc
-conda activate python37
+# source ~/.bashrc
+# conda activate python37
+alias python="~/miniconda3/bin/python"
 python ~/gitDocs/dataVisualization/python/get_wuhan_data.py >> wuhan_$time_now.txt
 
 mv *.txt ~/wuhan/
