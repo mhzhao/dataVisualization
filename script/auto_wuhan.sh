bfb67f1bd2a3963c3cb29996d530c9a343dc06ba
liyp0095 <liyp0095@gmail.com>
2020-02-08 01:38:21 -0600

    add

--- script/auto_wuhan.sh
+++ script/auto_wuhan.sh
@@ -1,5 +1,5 @@
 time_now=`date +%s`
 
-python /home/liyp/gitDocs/dataVisualization/python/get_wuhan_data.py >> wuhan_$time_now.txt
+python ~/gitDocs/dataVisualization/python/get_wuhan_data.py >> wuhan_$time_now.txt
 
 mv *.txt ~/wuhan/
