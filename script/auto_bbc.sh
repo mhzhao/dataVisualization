474b5258a2db8b43842ceabf4f42980902f38060
liyp0095 <liyp0095@gmail.com>
2020-02-08 14:47:43 -0600

    add mini python37

--- script/auto_bbc.sh
+++ script/auto_bbc.sh
@@ -1,6 +1,6 @@
 time_now=`date +%s`
 
-alias python="~/miniconda3/bin/python"
+alias python="~/miniconda3/envs/python37/bin/python"
 python ~/gitDocs/dataVisualization/python/get_bbc_news.py >> ~/news/bbc/bbc_$time_now.txt
 
 # mv *.txt ~/wuhan/
